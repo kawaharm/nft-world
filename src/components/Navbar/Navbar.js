09305df74939181a251b2c6c6d159233af9900f8
kawaharm <mjbk295@gmail.com>
2022-03-01 09:06:12 -0800

    removed links in Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -9,48 +9,49 @@ import {
 } from './NavbarStyle';
 
 const Navbar = (props) => {
+    console.log(props.isAuth);
+    console.log(props.handleLogout);
+
     return (
-        <>
-            <Nav>
-                <Bars />
-                <NavMenu>
-                    <h1 className="nav-logo">NFT World</h1>
-                    <NavLink to='/' >
-                        Home
-                    </NavLink>
-                    <NavLink to='/markets' >
-                        Markets
-                    </NavLink>
-                    <NavLink to='/rankings' >
-                        Rankings
-                    </NavLink>
-                    <NavLink to='/team' >
-                        About
-                    </NavLink>
-                </NavMenu>
-                {
-                    props.isAuth
-                        ?
-                        <NavMenu>
-                            <NavLink to="/profile">
-                                Profile
-                            </NavLink>
-                            <NavLink>
-                                <span onClick={props.handleLogout}>Logout</span>
-                            </NavLink>
-                        </NavMenu>
-                        :
-                        <NavMenu>
-                            <NavLink to="/signup">
-                                Sign Up
-                            </NavLink>
-                            <NavLink to="/login">
-                                Login
-                            </NavLink>
-                        </NavMenu>
-                }
-            </Nav>
-        </>
+        <Nav>
+            <Bars />
+            <NavMenu>
+                <h1 className="nav-logo">NFT World</h1>
+                <NavLink to='/' >
+                    Home
+                </NavLink>
+                <NavLink to='/markets' >
+                    Markets
+                </NavLink>
+                <NavLink to='/rankings' >
+                    Rankings
+                </NavLink>
+                <NavLink to='/team' >
+                    About
+                </NavLink>
+            </NavMenu>
+            {
+                props.isAuth
+                    ?
+                    <NavMenu>
+                        <NavLink to="/profile">
+                            Profile
+                        </NavLink>
+                        <NavLink>
+                            <span onClick={props.handleLogout}>Logout</span>
+                        </NavLink>
+                    </NavMenu>
+                    :
+                    <NavMenu>
+                        <NavLink to="/signup">
+                            Sign Up
+                        </NavLink>
+                        <NavLink to="/login">
+                            Login
+                        </NavLink>
+                    </NavMenu>
+            }
+        </Nav>
     );
 };
 
